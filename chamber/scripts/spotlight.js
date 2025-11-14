3bbf65935f90fc92a01abde26bc7a2f8a978b498
Neron56 <Michaelshaw20004@gmail.com>
2025-11-13 19:50:00 -0700

    week 3 submission

---
+++ chamber/scripts/spotlight.js
@@ -0,0 +1,61 @@
+fetchBusinesses()
+
+async function fetchBusinesses() {
+    const response = await fetch("http://127.0.0.1:5500/chamber/data/members.json");
+    const businesses = await response.json();
+    spotlightBusiness(businesses.businesses);
+}
+
+function spotlightBusiness(businesses) {
+    let businessCount = businesses.length;
+    let chosenBusinesses = []
+    while (chosenBusinesses.length != 3) {
+        let randomBusiness = Math.floor(Math.random() * businessCount);
+        let randomBusinessMembership = businesses[randomBusiness].membership;
+        if(randomBusinessMembership > 1){
+            if (!chosenBusinesses.includes(randomBusiness)) {
+            chosenBusinesses.push(randomBusiness)
+            }
+        }
+    }
+    chosenBusinesses.forEach(chosenId => {
+        businesses.forEach(business => {
+            let businessId = business.id;
+            if(chosenId == businessId){
+                let card = document.createElement("div");
+                let image = document.createElement("img");
+                let name = document.createElement("h3");
+                let address = document.createElement("p");
+                let number = document.createElement("p");
+                let url = document.createElement("a");
+                let membership = document.createElement("p")
+        
+                image.setAttribute("src",business.logo);
+                image.setAttribute("alt",`Business Logo for ${business.name}`);
+                image.setAttribute("loading","lazy");
+                image.setAttribute("class","businessImage");
+                name.innerHTML = `${business.name}`;
+                address.innerHTML = `${business.address}`;
+                number.innerHTML = `${business.number}`;
+                url.setAttribute("href", business.url);
+                url.innerHTML = `${business.url}`;
+                if(business.membership == 2){
+                    membership.innerHTML = `Membership level: Silver`;
+                }else{
+                    membership.innerHTML = `Membership level: Gold`;
+                }
+        
+                card.setAttribute("class","card");
+        
+                card.appendChild(image);
+                card.appendChild(name);
+                card.appendChild(address);
+                card.appendChild(number);
+                card.appendChild(url);
+                card.appendChild(membership);
+        
+                document.querySelector("#businessCards").appendChild(card);
+            }
+        });
+    });
+}
--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -1,6 +1,10 @@
+const latitude = -3.01;
+const longitude = 16.78;
+const apiKey = "ee5c4d8ea358463375cfeeabfcff2f70";
 getWeather()
+getForcast()
 async function getWeather() {
-    //const response = await fetch("https://api.openweathermap.org/data/2.5/weather?lat=16.78&lon=-3.01&appid=ee5c4d8ea358463375cfeeabfcff2f70&units=imperial");
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`);
     let weather = await response.json();
     let weatherData = document.createElement("div");
     let temp = document.createElement("p");
@@ -12,15 +16,53 @@ async function getWeather() {
     clouds.innerHTML = `${weather.weather[0].description}`;
     high.innerHTML = `High: ${weather.main.temp_max}`;
     low.innerHTML = `Low: ${weather.main.temp_min}`;
-    humidity.innerHTML = `Humidity: ${weather.main.humidity}%`;
-    sunrise.innerHTML = `Sunrise: ${weather.sys.sunrise}`;
-    sunset.innerHTML = `Sunset: ${weather.sys.sunset}`;
-
     weatherData.appendChild(temp);
     weatherData.appendChild(clouds);
     weatherData.appendChild(high);
     weatherData.appendChild(low);
 
     document.querySelector("#currentWeather").appendChild(weatherData);
+}
 
+async function getForcast() {
+    const response = await fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`)
+    let forcasts = await response.json();
+    let forcast = document.createElement("div")
+    let day1 = document.createElement("h3");
+    let day2 = document.createElement("h3");
+    let day3 = document.createElement("h3");
+
+    let twoDaysFromNow = getTwoDaysFromNow()
+    day1.innerHTML = `Today: ${forcasts.list[0].main.temp}℉`;
+    day2.innerHTML = `Tomorrow: ${forcasts.list[8].main.temp}℉`;
+    day3.innerHTML = `${twoDaysFromNow}: ${forcasts.list[16].main.temp}℉`;
+
+    forcast.appendChild(day1);
+    forcast.appendChild(day2);
+    forcast.appendChild(day3);
+    document.querySelector("#forcast").appendChild(forcast)
+}
+
+function getTwoDaysFromNow() {
+    let currentDate = new Date().getDay();
+    console.log(currentDate);
+    let twoDaysFromNow;
+    console.log(twoDaysFromNow);
+    switch(currentDate){
+        case 0:
+            twoDaysFromNow = "Sunday"
+        case 1:
+            twoDaysFromNow = "Tuesday"
+        case 2:
+            twoDaysFromNow = "Wednesday"
+        case 3:
+            twoDaysFromNow = "Thursday"
+        case 4:
+            twoDaysFromNow = "Friday"
+        case 5:
+            twoDaysFromNow = "Saturday"
+        case 6:
+            twoDaysFromNow = "Monday"
+    }
+    return twoDaysFromNow
 }
