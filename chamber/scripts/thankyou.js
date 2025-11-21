3098b46ba59bc0b1f92dd4d083fd740fe2f44542
Neron56 <Michaelshaw20004@gmail.com>
2025-11-20 20:59:51 -0700

    bug fix

--- chamber/scripts/thankyou.js
+++ chamber/scripts/thankyou.js
@@ -5,19 +5,16 @@ const organizationalTitle = params.get("organizationalTitle");
 const email = params.get("email");
 const phoneNumber = params.get("phoneNumber");
 const organization = params.get("businessName");
-const membershipLevel = params.get("membershipLevel");
-const description = params.get("textarea");
+const description = params.get("organizationDescription");
 const timestamp = params.get("timestamp");
 
 const information = document.querySelector("#information");
 
 
 information.innerHTML = `
-    Thank you ${organizationalTitle} ${fullName} from ${organization} for your application! Heres the information you gave:<br><br>
+    Thank you ${fullName} from ${organization} for your application! Heres the information you gave:<br><br>
     email: ${email}<br><br>
     phone number: ${phoneNumber}<br><br>
-    membership level: ${membershipLevel}<br><br>
-    description: ${description}<br><br>
     The form was loaded at: ${timestamp}<br><br>
     Thank You!
 `
